f56fd648139a9c8ac7074d793701b985b70b40b6
planetacomputer <evapardeza123@gmail.com>
2016-07-21 17:24:44 +0200

    JSON

--- json.php
+++ json.php
@@ -4,8 +4,8 @@ $data1 = array( 'a', 'b', 'c' );
 $data2 = array( 'name' => 'God', 'age' => -1 );
 $option = 1; 
 header('Content-type: application/json');
-if ( $option == 1 )
-  echo json_encode( $data1 );
+if ( $option == 2 )
+  echo json_encode( $data2 );
   // prints json array ["a","b","c"]
 else
   echo json_encode( $data2 );
